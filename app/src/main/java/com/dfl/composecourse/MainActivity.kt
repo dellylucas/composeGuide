3d58caee680ca73a7cd411ab6039e02a42dc20a8
dellylucas <dellylucas@hotmail.com>
2023-09-12 00:37:15 -0500

    first commit

--- app/src/main/java/com/dfl/composecourse/MainActivity.kt
+++ app/src/main/java/com/dfl/composecourse/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Delly Android")
                 }
             }
         }
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello Gato $name!",
         modifier = modifier
     )
 }
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ComposeCourseTheme {
-        Greeting("Android")
+        Greeting("Dell Android")
     }
 }
